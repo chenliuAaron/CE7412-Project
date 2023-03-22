a8f72cabaebb4a6e6f7aec257a8a74d7a6794532
Calvin Wang <53930614+kw108@users.noreply.github.com>
2023-03-22 13:15:09 +0800

    Create main.py
    Run main.py within the folder where the .fasta files are hosted and you will obtain k-mer encodings in nested list format.

---
+++ data/main.py
@@ -0,0 +1,49 @@
+if __name__ == '__main__':
+    # predefine k for k-mer encoding
+    k = 8
+    filenames = ['Dengue-4-sequences', 'Ebola-sequences', 'hepatitis-C-3a-sequences',
+                 'influenza-A-sequences', 'mers-sequences', 'SARS-CoV-2-sequences']
+
+    for filename in filenames:
+        with open(filename + '.fasta', 'r') as file:
+            # read DNA sequences into seqs
+            seqs = []
+            seq = ""
+            for line in file.readlines():
+                if line.startswith('>'):
+                    if len(seq) > 0:
+                        seqs.append(seq)
+                        seq = ""
+                else:
+                    seq += line.strip('\n')
+            if len(seq) > 0:
+                seqs.append(seq)
+
+            print('The file %s has %d sequences.\n' % (filename, len(seqs)))
+
+            # encode DNA sequences with k-mer
+            encodings = []
+            for seq in seqs:
+                encoding = []
+                code = 0
+                for c in seq:
+                    code *= 4
+                    if c == 'C':
+                        code += 1
+                    if c == 'G':
+                        code += 2
+                    if c == 'T':
+                        code += 3
+                    code %= 4 ** k
+                    encoding.append(code)
+                assert len(seq) == len(encoding), 'Error: Unmatched number of characters!'
+                encodings.append(encoding)
+            assert len(seqs) == len(encodings), 'Error: Unmatched number of sequences!'
+
+            import pickle
+            # dump encodings into .pkl files
+            with open(filename + '.pkl', 'wb') as pkfile:
+                pickle.dump(encodings, pkfile)
+            # load encodings from .pkl files
+            with open(filename + '.pkl', 'rb') as pkfile:
+                encodings = pickle.load(pkfile)
